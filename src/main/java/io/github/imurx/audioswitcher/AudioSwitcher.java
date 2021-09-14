447c3b74fe46d6763618afac5a1059a969f9960b
ImUrX <urielfontan2002@gmail.com>
2021-09-14 15:37:21 -0300

    testing openal

--- src/main/java/io/github/imurx/audioswitcher/AudioSwitcher.java
+++ src/main/java/io/github/imurx/audioswitcher/AudioSwitcher.java
@@ -1,12 +1,17 @@
 package io.github.imurx.audioswitcher;
 
 import net.fabricmc.api.ModInitializer;
+import org.lwjgl.openal.AL;
 import org.lwjgl.openal.ALC;
 import org.lwjgl.openal.ALC11;
+import org.lwjgl.openal.EnumerateAllExt;
 
 public class AudioSwitcher implements ModInitializer {
 	@Override
 	public void onInitialize() {
-
+		if(!ALC11.alcIsExtensionPresent(0, "ALC_ENUMERATE_ALL_EXT")) {
+			throw new IllegalStateException("Computer doesn't has the Enumerate All extension");
+		}
+		System.out.println(ALC11.alcGetString(0, EnumerateAllExt.ALC_ALL_DEVICES_SPECIFIER).replaceAll("\0", ","));
 	}
 }
